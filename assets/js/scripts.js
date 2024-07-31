d7bd3f061b7d1cf06257ce55c98a617ce95ff84c
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-07-31 15:44:39 +0200

    Add comments to JavaScript file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,15 @@
+// Initiate variables to get dropdown icon and navigation menu
+
 const dropdownIcon = document.querySelector("#dropdown-icon");
 const navMenu = document.querySelector("#mobile-nav");
+
+// Add nav-active class once dropdownIcon is clicked
 
 dropdownIcon.addEventListener("click", () => {
     navMenu.classList.toggle("nav-active");
 })
+
+// Remove navigation menu once the window is resized
 
 addEventListener("resize", () => {
     navMenu.classList.remove("nav-active");
