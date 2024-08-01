33588cb734d495ac4c208e0d277f87e97e87d138
Dominik Wulf <dominik.wulf@students.codeinstitute.net>
2024-08-01 11:01:54 +0200

    Add basic dropdown script to adventure page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,3 +14,21 @@ dropdownIcon.addEventListener("click", () => {
 addEventListener("resize", () => {
     navMenu.classList.remove("nav-active");
 })
+
+// Adventure dropdown menu
+
+function openDropdown(dropdownMenu) {
+
+    const clickedAdventure = dropdownMenu.dataset.adventure;
+    const clickedButton = dropdownMenu.dataset.button;
+    const dropdownElements = document.querySelectorAll(".adventure-dropdown");
+
+    for (let i = 0; i < dropdownElements.length; i++) {
+        if (dropdownElements[i].dataset.button == clickedButton && dropdownElements[i].dataset.adventure == clickedAdventure) {
+            dropdownElements[i].classList.toggle("adventure-dropdown-shown");
+        }
+    }
+
+
+
+}
